dbff0d659da6f2400fc569e2fd1ce6115facee14
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-05-09 13:50:45 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Entscheidungsstrukturen | control flow
+
+// Deklaration + Assigment
+const ageJohn = 30;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
